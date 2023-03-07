bb2960a5fd76abd826d8202a888fb5160ccba7ea
MatiasOrlando <matiassorlando@gmail.com>
2023-03-07 21:10:14 +0100

    feat: asociacion tabla products y customers

--- back/models/index.js
+++ back/models/index.js
@@ -3,4 +3,6 @@ const Carrito = require("./Carrito");
 const Customer = require("./Customer");
 const Product = require("./Product");
 
+Product.belongsToMany(Customer, { through: Carrito });
+
 module.exports = { Brand, Carrito, Customer, Product };
