efa751626e5a01a05e5ccf69c16259a678af80af
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-03-07 17:08:09 -0300

    feat: asociaciones

--- back/models/index.js
+++ back/models/index.js
@@ -2,5 +2,13 @@ const Brand = require("./Brand");
 const Carrito = require("./Carrito");
 const Customer = require("./Customer");
 const Product = require("./Product");
+const Order = require("./Order");
+
+Order.belongsTo(Customer);
+Customer.hasMany(Order);
+
+Product.belongsTo(Brand);
+Brand.hasMany(Product);
+
 
 module.exports = { Brand, Carrito, Customer, Product };
