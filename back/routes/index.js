296e99bf527c15d52011c8e4612642fc503a49dd
MatiasOrlando <matiassorlando@gmail.com>
2023-03-08 19:46:40 +0100

    feat: rutas getAllProducts y getSingleProduct agregadas

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const usersRouter = require("./usersRoutes");
+const productRouter = require("./productRoutes");
 
 router.use("/users", usersRouter);
-
-
+router.use("/products", productRouter);
 
 module.exports = router;
