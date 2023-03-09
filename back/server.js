4d7b656455dbba225b43efdb6a4958cdd452567b
cristian-23 <cristianblancojr@gmail.com>
2023-03-09 11:16:27 -0400

    feat:cookies cors

--- back/server.js
+++ back/server.js
@@ -3,9 +3,14 @@ const app = express();
 const db = require("./db/db");
 const models = require("./models");
 const router = require("./routes/index");
+const cors = require("cors")
+const cookies = require("cookie-parser")
 
+app.use(cookies())
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
+
 
 db.sync({ force: false }).then(() => {
   app.listen(3001, () => console.log("Server ON PORT: 3001"));
