f7b05f5d6731e6243be50b6025024e36db164f60
MatiasOrlando <matiassorlando@gmail.com>
2023-03-10 14:17:15 +0100

    feat: ruta pagination added

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const usersRouter = require("./usersRoutes");
 const productRouter = require("./productRoutes");
-const searchRouter = require("./searchRouter");
+const searchRouter = require("./searchRoutes");
 const carritoRouter = require("./carritoRoutes");
+const paginationRouter = require("./paginationRoutes");
 
 router.use("/users", usersRouter);
 router.use("/products", productRouter);
 router.use("/search", searchRouter);
 router.use("/carrito", carritoRouter);
+router.use("/page", paginationRouter);
 
 module.exports = router;
