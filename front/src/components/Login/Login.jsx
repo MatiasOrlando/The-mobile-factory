8681645aed9c71e797b1d8bec9514ce2ad8f9d2b
GuidoBonesi <guidobonesi@gmail.com>
2023-03-14 09:53:55 -0300

    feat: validation

--- front/src/components/Login/Login.jsx
+++ front/src/components/Login/Login.jsx
@@ -3,14 +3,39 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../../state/user";
-import { Box, Button, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography, Alert } from "@mui/material";
 import { cartProducts, loginProducts } from "../../state/products";
 
 export const Login = (user) => {
+  console.log(user);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
+  // Input Error
+
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  // Email Validation
+  const isEmail = (email) =>
+    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+  // Validation for OnBlur password
+
+  const handlePassword = (e) => {
+    e.preventDefault();
+    if (!password || password.length < 4) {
+      setPasswordError(true);
+      return;
+    }
+    setPasswordError(false);
+  };
+
+  //Validation Form State
+
+  const [formValid, setFormValid] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,17 +45,20 @@ export const Login = (user) => {
         { email, password },
         { withCredentials: true }
       );
-      
+
       localStorage.setItem("user", JSON.stringify(userLog.data));
       dispatch(setUser(userLog.data));
-      const userCarrito = await axios.get(`http://localhost:3001/carrito/${userLog.data.id}`)
+      const userCarrito = await axios.get(
+        `http://localhost:3001/carrito/${userLog.data.id}`
+      );
       if (typeof userCarrito.data !== "string") {
         dispatch(loginProducts(userCarrito.data));
       }
       navigate("/");
     } catch (error) {
       console.error(error);
-      alert("error login");
+      setFormValid("Email o contraseña incorrecta");
+      return;
     }
   };
 
@@ -61,7 +89,7 @@ export const Login = (user) => {
               sx={{ marginBottom: "1rem" }}
             />
             <TextField
-              label="Password"
+              label="Contraseña"
               type="password"
               fullWidth
               value={password}
@@ -71,6 +99,7 @@ export const Login = (user) => {
             <Button variant="contained" type="submit" fullWidth>
               Login
             </Button>
+            <p>{formValid && <Alert severity="error">{formValid}</Alert>}</p>
           </form>
         </Box>
       </Box>
