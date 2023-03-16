194435a9fdf1cf6989d1f13a4aa683598ff6394c
German <german.1990.cuevas@gmail.com>
2023-03-16 14:27:58 -0300

    fix : formateo de la fecha en orders

--- front/src/components/ShoppingHistory/ShoppingHistory.jsx
+++ front/src/components/ShoppingHistory/ShoppingHistory.jsx
@@ -13,24 +13,24 @@ import { useSelector } from "react-redux";
 
 function ShoppingHistory() {
   const [history, setHistory] = useState([]);
-  const [histProds, setHistProds] = useState([])
+  const [histProds, setHistProds] = useState([]);
   //const user = useSelector((state) => state.user);
-  let user = JSON.parse(localStorage.getItem("user"))
+  let user = JSON.parse(localStorage.getItem("user"));
 
   useEffect(() => {
     async function fetchDevices() {
-        const response = await fetch(
+      const response = await fetch(
         `http://localhost:3001/checkout/ordersOneUser/${user.id}`
       );
       const data = await response.json();
-      
-      let arr = []
-      let miData = data.map(el=>el.products)
-      miData.forEach((el)=>{
-        el.forEach(e=>arr.push(JSON.parse(e)))
-      })
-      setHistProds(arr)
-      setHistory(data)
+
+      let arr = [];
+      let miData = data.map((el) => el.products);
+      miData.forEach((el) => {
+        el.forEach((e) => arr.push(JSON.parse(e)));
+      });
+      setHistProds(arr);
+      setHistory(data);
     }
     fetchDevices();
   }, []);
@@ -39,7 +39,6 @@ function ShoppingHistory() {
   let miMap = miArr.map(prod=>JSON.parse(prod))
   console.log(miMap); */
 
-
   const purchases = [
     { date: "2022-03-10", articulo: "Shoes", quantity: 1, precio: 50 },
     { date: "2022-03-11", articulo: "T-shirt", quantity: 2, precio: 20 },
@@ -47,62 +46,77 @@ function ShoppingHistory() {
   ];
   return (
     <div style={{ marginTop: "70px" }}>
-      {<TableRow
-        sx={{ width: "100%", display: "flex", justifyContent: "center" }}
-      >
-        <TableCell
-          style={{ paddingBottom: 0, paddingTop: 0, width: "75%" }}
-          colSpan={6}
+      {
+        <TableRow
+          sx={{ width: "100%", display: "flex", justifyContent: "center" }}
         >
-          <Box sx={{ margin: 1 }}>
-            <Typography
-              variant="h6"
-              gutterBottom
-              component="div"
-              sx={{
-                fontFamily: "monospace",
-                fontSize: "120%",
-                fontWeight: "900",
-                marginBottom: "2.5%",
-              }}
-            >
-              Historial de compras/ventas
-            </Typography>
-            <Table size="small" aria-label="purchases" sx={{}}>
-              <TableHead>
-                <TableRow>
-                  <TableCell sx={{ fontWeight: "bold" }}>Fecha</TableCell>
-                  <TableCell sx={{ fontWeight: "bold" }}>Articulo</TableCell>
-                  <TableCell align="right" sx={{ fontWeight: "bold" }}>
-                    Precio
-                  </TableCell>
-                  <TableCell align="right" sx={{ fontWeight: "bold" }}>
-                    Catidad
-                  </TableCell>
-                  <TableCell align="right" sx={{ fontWeight: "bold" }}>
-                    Precio Total ($)
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {histProds.map((purchase, i) => (
-                  <TableRow key={i}>
-                    <TableCell component="th" scope="row">
-                      {purchase.updatedAt}
+          <TableCell
+            style={{ paddingBottom: 0, paddingTop: 0, width: "75%" }}
+            colSpan={6}
+          >
+            <Box sx={{ margin: 1 }}>
+              <Typography
+                variant="h6"
+                gutterBottom
+                component="div"
+                sx={{
+                  fontFamily: "monospace",
+                  fontSize: "120%",
+                  fontWeight: "900",
+                  marginBottom: "2.5%",
+                }}
+              >
+                Historial de compras/ventas
+              </Typography>
+              <Table size="small" aria-label="purchases" sx={{}}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell sx={{ fontWeight: "bold" }}>Fecha</TableCell>
+                    <TableCell sx={{ fontWeight: "bold" }}>Articulo</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                      Precio
                     </TableCell>
-                    <TableCell>{purchase.productId}</TableCell>
-                    <TableCell align="right">{purchase.price}</TableCell>
-                    <TableCell align="right">{purchase.quantity}</TableCell>
-                    <TableCell align="right">
-                      { (parseFloat(purchase.price) * purchase.quantity).toFixed(2)}
+                    <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                      Catidad
+                    </TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                      Precio Total ($)
                     </TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Box>
-        </TableCell>
-      </TableRow>}
+                </TableHead>
+                <TableBody>
+                  {histProds.map((purchase, i) => (
+                    <TableRow key={i}>
+                      <TableCell component="th" scope="row">
+                        {new Date(purchase.updatedAt).toLocaleDateString(
+                          "es-AR",
+                          { year: "2-digit", month: "2-digit", day: "2-digit" }
+                        ) +
+                          " " +
+                          new Date(
+                            "2023-03-16T16:18:10.462Z"
+                          ).toLocaleTimeString("es-AR", {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          }) +
+                          " hs"}
+                      </TableCell>
+                      <TableCell>{purchase.productId}</TableCell>
+                      <TableCell align="right">{purchase.price}</TableCell>
+                      <TableCell align="right">{purchase.quantity}</TableCell>
+                      <TableCell align="right">
+                        {(
+                          parseFloat(purchase.price) * purchase.quantity
+                        ).toFixed(2)}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </TableCell>
+        </TableRow>
+      }
     </div>
   );
 }
